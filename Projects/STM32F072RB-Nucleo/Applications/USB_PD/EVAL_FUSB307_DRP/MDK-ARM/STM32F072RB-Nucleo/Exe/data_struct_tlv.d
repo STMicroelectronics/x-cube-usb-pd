7e29788399be7d522659b01377ef0ad6259e4cb4
frq07466 <yohann.martiniault@st.com>
2020-02-25 15:36:45 +0100

    Remove useless files

--- Projects/STM32F072RB-Nucleo/Applications/USB_PD/EVAL_FUSB307_DRP/MDK-ARM/STM32F072RB-Nucleo/Exe/data_struct_tlv.d
+++
@@ -1,4 +0,0 @@
-stm32f072rb-nucleo\exe\data_struct_tlv.o: ..\..\..\..\..\..\Utilities\GUI_INTERFACE\data_struct_tlv.c
-stm32f072rb-nucleo\exe\data_struct_tlv.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-stm32f072rb-nucleo\exe\data_struct_tlv.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-stm32f072rb-nucleo\exe\data_struct_tlv.o: ..\..\..\..\..\..\Utilities\GUI_INTERFACE\data_struct_tlv.h
--- Projects/STM32F072RB-Nucleo/Applications/USB_PD/EVAL_FUSB307_DRP/MDK-ARM/STM32F072RB-Nucleo/Exe/startup_stm32f072xb.d
+++
@@ -1,1 +0,0 @@
-stm32f072rb-nucleo\exe\startup_stm32f072xb.o: startup_stm32f072xb.s
