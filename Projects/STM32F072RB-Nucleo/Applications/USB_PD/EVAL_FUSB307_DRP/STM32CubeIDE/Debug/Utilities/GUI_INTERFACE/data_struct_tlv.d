27603990bffa1d64bf99d1b8ab19228c23a74ab6
frq07466 <yohann.martiniault@st.com>
2020-03-02 13:41:14 +0100

    LD file need to be renamed to generate correctly the FW image on STM32CubeIDE

--- Projects/STM32F072RB-Nucleo/Applications/USB_PD/EVAL_FUSB307_DRP/STM32CubeIDE/Debug/Utilities/GUI_INTERFACE/data_struct_tlv.d
+++
@@ -1,5 +0,0 @@
-Utilities/GUI_INTERFACE/data_struct_tlv.o: \
- C:/git/_Delivery/F0/GIT/Firmware/Utilities/GUI_INTERFACE/data_struct_tlv.c \
- C:/git/_Delivery/F0/GIT/Firmware/Utilities/GUI_INTERFACE/data_struct_tlv.h
-
-C:/git/_Delivery/F0/GIT/Firmware/Utilities/GUI_INTERFACE/data_struct_tlv.h:
